9bd2e9988b69d905b82ce1b80a8642ada368d064
helmus <dhwillem@gmail.com>
2012-04-14 20:28:31 +0200

    initialize types in constructor to prevent null values
    typo fix

--- JsonSettingsDemo/DemoSettings.cs
+++ JsonSettingsDemo/DemoSettings.cs
@@ -4,13 +4,18 @@ namespace JsonSettingsDemo
 {
     public class DemoSettings : Settings
     {
+        public DemoSettings()
+        {
+            // initialize to prevent null values
+            exclude = new List<string>();
+            someMap = new Dictionary<string, string>();
+            apiUri = "";
+        }
+
         protected override void defaults()
         {
             apiUri = "http://example.com";
             waitInterval = 1500;
-
-            exclude = new List<string>();
-            someMap = new Dictionary<string, string>();
 
             exclude.Add("value1");
             someMap.Add("example", "exampleValue");
@@ -18,7 +23,7 @@ namespace JsonSettingsDemo
         }
 
         
-        /* Adding a new config value is as simple as ading a new public field or property
+        /* Adding a new config value is as simple as adding a new public field or property
          * You don't need to add it to the config file,
          * The config file will be auto updated with the new value on the next run
          */
